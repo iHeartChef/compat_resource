e7f791f99e96c584238055af1ceb6662d3f36a07
Jesse Callaway <JesseCallaway@iHeartMedia.com>
2015-11-04 17:44:35 -0500

    fix metadata problems

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,6 @@ require_relative 'files/lib/chef_compat/version'
 version ChefCompat::VERSION
 
 chef_version ">= 12.0" if respond_to?(:chef_version)
+# Iheart fixes
+maintainer 'upstream'
+maintainer_email 'nobody@upstream'
